4ce85ec3428e59c23467c67d7b18836302addd10
Tumen Virt <bairovt@gmail.com>
2016-11-28 17:51:31 +0900

    текучка

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const arangojs = require('arangojs');
 const render = require('koa-swig');
 const path = require('path');
+const Router = require('koa-router');
+const aql = arangojs.aql;
 
 const app = new Koa();
 app.context.render = render({
@@ -12,19 +14,29 @@ app.context.render = render({
 	// locals: locals
 })
 
+const router = new Router();
+
 const db = new arangojs({
 	url: 'http://root:ultra@127.0.0.1:8529',
 	databaseName: 'rod'
 });
 
 
-app.use(function *(next) {
-	let personsColl = db.collection('Persons');
-	let cursor = yield personsColl.all();
+router.get('/', function *(next) {
+	// let personsColl = db.collection('Persons');
+	// let cursor = yield personsColl.all();
+	let cursor = yield db.query(aql`FOR p IN Persons SORT p.fullname RETURN p`);
 	let persons = yield cursor.all();
-	yield this.render("index", { persons: 'bla' });
-	// yield this.render("layout.html");
+	yield this.render("index", { persons });	
 });
 
-app.listen(3000);
-console.log('Listening on 3000')
+router.get('/:_key', function *(next) {
+	let personsColl = db.collection('Persons');
+	let person = yield personsColl.document(this.params._key);	
+	yield this.render("person", { person });	
+});
+
+app.use(router.routes());
+
+app.listen(5000);
+console.log('Listening on 5000')
