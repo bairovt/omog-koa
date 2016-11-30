81b87affffb25c0bb8b5aff7b4aee399a9230f19
Tumen Virt <bairovt@gmail.com>
2016-11-30 18:19:09 +0900

    Вывод предков списком

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.context.render = render({
 	root: __dirname + '/templates',//path.join(__dirname, 'templates'),
 	autoescape: true,
 	cache: false, //memory, disable, set to false
-	ext: 'html',
+	ext: 'html'
 	// locals: locals
-})
+});
 
 const router = new Router();
 
@@ -33,28 +33,32 @@ router.get('/', function *(next) {
 // Personal page
 router.get('/:_key', function *(next) {
 	// let personsColl = db.collection('Persons');
-	// let person = yield personsColl.document(this.params._key);
-	// let 
+	// let person = yield personsColl.document(this.params._key);	
+	let _id = `Persons/${this.params._key}`;	
+
 	let cursor = yield db.query(
 		aql`FOR v, e, p
 		    IN 0..100 INBOUND
-		    "Persons/${this.params._key}"
-		    GRAPH "parentGraph"
+		    ${_id}
+		    GRAPH 'parentGraph'
 		    OPTIONS {bfs: true}
 		    RETURN {person: v, edges: p.edges}`
     );
+    //console.log(cursor);
     let ancestors = yield cursor.all(); // ancestors[0] is person
-    let person = ancestors[0];
-    // формируем объект массивов поколений: ключ - глубина колена, значение - массив предков этого колена
-    let gens = ancestors.reduce(function(gens, current, index) {
-    	if (index == 0) return res; // игнорируем 0-й элемент (person)
-    	let i = current.edges.length; // глубина колена текущего предка
-    	if (typeof res[i] === 'undefined') res[i] = []; // инициируем массив предков i-го колена, если его нет
-    	res[i].push(cursor.fullname); // добавляем предка в массив предков i-го колена
-    	return res;
-    }, {}); // на входе пустой объект
-    let gensCount = Object.keys(gens).length;
-	yield this.render("person", { person, gens, gensCount });
+    
+    let person = ancestors[0].person;
+    // // формируем объект массивов поколений: ключ - глубина колена, значение - массив предков этого колена
+    // let gens = ancestors.reduce(function(gens, current, index) {
+    // 	if (index == 0) return gens; // игнорируем 0-й элемент (person)
+    // 	let i = current.edges.length; // глубина колена текущего предка
+    // 	if (typeof gens[i] === 'undefined') gens[i] = []; // инициируем массив предков i-го колена, если его нет
+    // 	gens[i].push(cursor.fullname); // добавляем предка в массив предков i-го колена
+    // 	return gens;
+    // }, {}); // на входе пустой объект
+    // let gensCount = Object.keys(gens).length;
+
+	yield this.render("person", { person, ancestors }); //gens, gensCount
 });
 
 app.use(router.routes());
