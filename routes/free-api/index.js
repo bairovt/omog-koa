2f9e36cf2212cfb11e8243a1276dbbf45492defd
Tumen Bairov <bairovt@gmail.com>
2017-03-19 14:44:58 +0900

    refactor: Contibs -> HeroContribs

--- routes/free-api/index.js
+++ routes/free-api/index.js
@@ -7,7 +7,7 @@ const utils = require('utils');
 const router = new Router();
 
 async function getHeroContribs(ctx, next) {
-  const contribs = await db.query(aql`FOR c IN Contribs SORT c.date RETURN c`).then(cursor => cursor.all());
+  const contribs = await db.query(aql`FOR c IN HeroContribs SORT c.date RETURN c`).then(cursor => cursor.all());
   // console.log(typeof(contribs));
   ctx.set('Access-Control-Allow-Origin', '*'); // allow cross site request (for requests from hero.rod.so)
   return ctx.body = {contribs};
