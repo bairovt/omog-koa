cea7af92af41bb0d5d9f4af58540745560f92635
Tumen Bairov <bairovt@gmail.com>
2018-01-11 13:43:18 +0900

    start prod & dev sh scripts

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export NODE_ENV=production
+export NODE_ENV=development
 export NODE_PATH=/home/tumen/nodejs/rod.so
 cd /home/tumen/nodejs/rod.so
 #node --harmony-async-await main-rod.js &
-node main-rod.js &
+node main-rod.js
---
+++ start_prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export NODE_ENV=production
+export NODE_PATH=/home/tumen/nodejs/rod.so
+cd /home/tumen/nodejs/rod.so
+#node --harmony-async-await main-rod.js &
+node main-rod.js &
