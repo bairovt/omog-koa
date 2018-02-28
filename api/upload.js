ca63bbcfab373e97414c89a39fd6ce805aa7a7d6
Tumen Bairov <bairovt@gmail.com>
2018-02-28 12:02:27 +0900

    upload permission to manager

--- api/upload.js
+++ api/upload.js
@@ -53,7 +53,7 @@ async function prepare(ctx, next) {
   // todo: person_key verify, perms
   const {person_key} = ctx.params
   const person = await fetchPerson(person_key) // or throw 404
-  if (await checkPermission(ctx.state.user, person)) {
+  if (await checkPermission(ctx.state.user, person, {manager: true})) {
     const uploadDir = path.join(config.get('uploadDir'), person_key)
     let stats;
     try {
