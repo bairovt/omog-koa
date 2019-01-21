d58c716ce9f1abd0abeb6a193b02dab9d5498a51
Tumen <bairovt@gmail.com>
2019-01-21 15:23:11 +0700

    fix upload dir

--- config/default.js
+++ config/default.js
@@ -17,7 +17,7 @@ module.exports = {
       user: 'rod_base_user',
       password: 'rod_base_pass'
     },
-    uploadDir: '/home/tumen/vuejs/omogme/static/upload',
+    uploadDir: '/home/tumen/vuejs/omogme/dist/static/upload',
     mailer: {
         user: 'mail@omog.info',
         pass: 'password'
