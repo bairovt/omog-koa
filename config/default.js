c3d718f9a7d45095f1495f9ce17c6dec095303d4
Tumen <bairovt@gmail.com>
2019-02-15 22:17:00 +0700

    db.name to config

--- config/default.js
+++ config/default.js
@@ -3,23 +3,24 @@ const path = require('path');
 const root = process.cwd();
 
 module.exports = {
-    // secret data can be moved to env variables
-    // or a separate config
-    secretKeys: ['super creazy secret key'],
-    root: root,
-    server: {
-        port: 8000
-    },
-    env: 'development',
-    corsOrigin: 'http://localhost:8080',
-    db: {
-      URL: 'http://127.0.0.1:8529',
-      user: 'rod_base_user',
-      password: 'rod_base_pass'
-    },
-    uploadDir: '/home/tumen/vuejs/omogme/dist/static/upload',
-    mailer: {
-        user: 'mail@omog.info',
-        pass: 'password'
-    }
+  // secret data can be moved to env variables
+  // or a separate config
+  secretKeys: ['super creazy secret key'],
+  root: root,
+  server: {
+    port: 8000
+  },
+  env: 'development',
+  corsOrigin: 'http://localhost:8080',
+  db: {
+    URL: 'http://127.0.0.1:8529',
+    name: 'rod_database_name',
+    user: 'rod_database_user',
+    password: 'rod_database_pass'
+  },
+  uploadDir: '/home/tumen/vuejs/omogme/dist/static/upload',
+  mailer: {
+    user: 'mail@omog.info',
+    pass: 'password'
+  }
 };
